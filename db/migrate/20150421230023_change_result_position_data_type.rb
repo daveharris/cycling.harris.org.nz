2c86d3c7c3ac0ca363e87f1037e274381f727640
Dave Harris <dave@harris.org.nz>
2015-04-23 09:05:29 +1200

    Added SQL ALTER TABLE USING CAST for data types

--- db/migrate/20150421230023_change_result_position_data_type.rb
+++ db/migrate/20150421230023_change_result_position_data_type.rb
@@ -3,8 +3,13 @@ class ChangeResultPositionDataType < ActiveRecord::Migration
     change_column :results, :timing_url, :string
     change_column :results, :strava_url, :string
 
-    change_column :results, :position, :integer
-    change_column :results, :finishers, :integer
+    if ActiveRecord::Base.retrieve_connection.kind_of?(ActiveRecord::ConnectionAdapters::PostgreSQLAdapter)
+      execute 'ALTER TABLE results ALTER COLUMN position TYPE integer USING CAST(position as INTEGER);'
+      execute 'ALTER TABLE results ALTER COLUMN finishers TYPE integer USING CAST(finishers as INTEGER);'
+    else
+      change_column :results, :position, :integer
+      change_column :results, :finishers, :integer
+    end
 
     Result.all.each { |r| r.update_attributes(position: 0) if r.position.nil? }
     Result.all.each { |r| r.update_attributes(finishers: 0) if r.finishers.nil? }
