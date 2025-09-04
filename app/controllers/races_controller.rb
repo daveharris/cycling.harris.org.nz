d0f11a09b5279619856e9f09decd1557ecadf6d7
Dave Harris <dave@harris.org.nz>
2025-09-04 19:04:52 +1200

    Add Races

---
+++ app/controllers/races_controller.rb
@@ -0,0 +1,50 @@
+class RacesController < ApplicationController
+  before_action :set_race, only: %i[show edit update destroy]
+
+  def index
+    @races = Race.alphabetical
+  end
+
+  def show
+  end
+
+  def new
+    @race = Race.new
+  end
+
+  def edit
+  end
+
+  def create
+    @race = Race.new(race_params)
+
+    if @race.save
+      redirect_to @race, notice: "#{@race} was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @race.update(race_params)
+      redirect_to @race, notice: "#{@race} was successfully updated.", status: :see_other
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @race.destroy!
+    redirect_to races_path, notice: "#{@race} was successfully destroyed.", status: :see_other
+  end
+
+  private
+
+  def set_race
+    @race = Race.find(params.expect(:id))
+  end
+
+  def race_params
+    params.expect(race: [:name, :distance])
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,10 @@
 User.create(email_address: "dave@harris.org.nz", password: "password")
 User.create(email_address: "ian@harris.org.nz", password: "password")
 puts "Created #{User.count} Users"
+
+Race.create(name: "Wairarapa Cycle Challenge, Featherston", distance: 80)
+Race.create(name: "Tour of Waikanae", distance: 100)
+Race.create(name: "Martinborough Charity Fun Ride", distance: 115)
+Race.create(name: "Tour of the Wairarapa, Masterton", distance: 115)
+Race.create(name: "Taupo Cycle Challenge", distance: 160)
+puts "Created #{Race.count} Race"
