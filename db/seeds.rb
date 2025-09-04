6b42368a7b032968fce6386b4e7bbde77a26d8b1
Dave Harris <dave@harris.org.nz>
2025-09-04 20:34:44 +1200

    Generate Rails authentication, keep only session

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,8 +2,7 @@
 # development, test). The code here should be idempotent so that it can be executed at any point in every environment.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+
+User.create(email_address: "dave@harris.org.nz", password: "password")
+User.create(email_address: "ian@harris.org.nz", password: "password")
+puts "Created #{User.count} Users"
