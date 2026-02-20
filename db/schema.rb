90f73ab2bac8506d93c2eb54e15e8d293bb503e3
Dave Harris <dave@harris.org.nz>
2026-02-20 15:26:46 +1300

    Add name to  User

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_09_04_092640) do
+ActiveRecord::Schema[8.1].define(version: 2026_02_23_001238) do
   create_table "races", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.integer "distance"
@@ -48,6 +48,7 @@ ActiveRecord::Schema[8.1].define(version: 2025_09_04_092640) do
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "email_address", null: false
+    t.string "name", null: false
     t.string "password_digest", null: false
     t.datetime "updated_at", null: false
     t.index ["email_address"], name: "index_users_on_email_address", unique: true
