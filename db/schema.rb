631023e383a8e64aca31dd949607c4ed8d8c359c
Dave Harris <dave@harris.org.nz>
2026-02-23 13:08:22 +1300

    Rails 8 re-orders db/schema.rb alphabetically (no change)

--- db/schema.rb
+++ db/schema.rb
@@ -10,45 +10,45 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_09_04_092640) do
+ActiveRecord::Schema[8.1].define(version: 2025_09_04_092640) do
   create_table "races", force: :cascade do |t|
-    t.string "name"
-    t.integer "distance"
     t.datetime "created_at", null: false
+    t.integer "distance"
+    t.string "name"
     t.datetime "updated_at", null: false
   end
 
   create_table "results", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.integer "race_id", null: false
-    t.integer "duration"
-    t.date "date"
     t.text "comment"
-    t.string "url"
-    t.string "wind"
+    t.datetime "created_at", null: false
+    t.date "date"
+    t.integer "duration"
     t.integer "fastest_duration"
+    t.integer "finishers"
     t.integer "median_duration"
     t.integer "position"
-    t.integer "finishers"
-    t.datetime "created_at", null: false
+    t.integer "race_id", null: false
     t.datetime "updated_at", null: false
+    t.string "url"
+    t.integer "user_id", null: false
+    t.string "wind"
     t.index ["race_id"], name: "index_results_on_race_id"
     t.index ["user_id"], name: "index_results_on_user_id"
   end
 
   create_table "sessions", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.string "ip_address"
-    t.string "user_agent"
     t.datetime "created_at", null: false
+    t.string "ip_address"
     t.datetime "updated_at", null: false
+    t.string "user_agent"
+    t.integer "user_id", null: false
     t.index ["user_id"], name: "index_sessions_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
     t.string "email_address", null: false
     t.string "password_digest", null: false
-    t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email_address"], name: "index_users_on_email_address", unique: true
   end
